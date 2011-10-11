1cbca7e66ceb9689ab6784985b01df97616ed0c2
dexter <dexter@ed2ff44e-3bad-47df-acac-40d95bfd9e26>
2011-10-11 20:47:41 +0000

    new subprocess spawning tests

--- trunk/framework/src/tests/refactor/test_permutations.py
+++ trunk/framework/src/tests/refactor/test_permutations.py
@@ -9,99 +9,110 @@ import traceback
 import time
 import unittest
 import logging
+import string
+import subprocess
+from test_parameterized_cases import ParameterizedTestCase
 
 sys.path.append('../..')
 
 from ips import Framework
 from configobj import ConfigObj
 
-class test_permutations(unittest.TestCase):
-
-    def __init__(self, methodName='runTest',
-                 do_create_runspace=True, do_run_setup=True, do_run=True,
-                 create_runspace_done=True, run_setup_done=True, run_done=True):
-        super(test_permutations, self).__init__(methodName)
-        self.do_create_runspace = do_create_runspace
-        self.do_run_setup = do_run_setup
-        self.do_run = do_run
-        self.create_runspace_done = create_runspace_done
-        self.run_setup_done = run_setup_done
-        self.run_done = run_done
+class test_permutations(ParameterizedTestCase):
 
     def printUsageMessage(self):
         print 'Usage: ips [--create-runspace | --run-setup | --run]+ --simulation=SIM_FILE_NAME --platform=PLATFORM_FILE_NAME --log=LOG_FILE_NAME [--debug | --ftb]'
 
-    def suite(self):
-        return unittest.TestSuite(map(test_permutations, ['runTest']))
-
-    def runTest(self):
-        print
-        cfgFile_list = []
-        cfgFile_list.append('basic_serial1_iter.conf')
-        platform_filename = 'iter.conf'
-        log_file = 'log_test_basic_serial1_on_iter.log'
-        #log_file = open(os.path.abspath('log_test_basic_serial1_on_iter.log'), 'w')
-        #log_file = sys.stdout
-
+    def setUp(self):
         # create framework with config file
+        print
         print '------------------------------------------------------------------------------------'
         print 'Parameterization for this test'
         print 'DO_CREATE_RUNSPACE = %s DO_RUN_SETUP = %s DO_RUN = %s' % \
-            (self.do_create_runspace, self.do_run_setup, self.do_run)
+            (self.param.do_create_runspace, self.param.do_run_setup, self.param.do_run)
         print 'CREATE_RUNSPACE_DONE = %s RUN_SETUP_DONE = %s RUN_DONE = %s' % \
-            (self.create_runspace_done, self.run_setup_done, self.run_done)
+            (self.param.create_runspace_done, self.param.run_setup_done, self.param.run_done)
         print '------------------------------------------------------------------------------------'
-        fwk = Framework(self.do_create_runspace, 
-                        self.do_run_setup,
-                        self.do_run, 
-                        cfgFile_list, 
-                        log_file, 
-                        platform_filename)
-        checklist_file_name = os.path.join(fwk.sim_root, 'checklist.conf')
+#       self.fwk = Framework(self.param.do_create_runspace, 
+#                       self.param.do_run_setup,
+#                       self.param.do_run, 
+#                       self.param.cfgFile_list, 
+#                       self.param.log_file, 
+#                       self.param.platform_filename)
+
+#   def tearDown(self):
+#       self.fwk = None
+
+    def test_given_parameters(self):
+
+        # create the checklist.conf that Framework will use
+#       checklist_file_name = os.path.join(self.fwk.sim_root, 'checklist.conf')
+        sim_conf = ConfigObj(self.param.cfgFile_list[0], interpolation='template',
+                         file_error=True)
+        checklist_file_name = os.path.join(sim_conf['SIM_ROOT'], 'checklist.conf')
+#       checklist_file_name = os.path.join('/Users/dx4/all_runs/test_basic_serial1_0/', 'checklist.conf')
         checklist_file = open(checklist_file_name, 'w')
-        if self.create_runspace_done:
+        if self.param.create_runspace_done:
             checklist_file.write('CREATE_RUNSPACE = DONE\n')
         else:
             checklist_file.write('CREATE_RUNSPACE = NOT_DONE\n')
-        if self.run_setup_done:
+        if self.param.run_setup_done:
             checklist_file.write('RUN_SETUP = DONE\n')
         else:
             checklist_file.write('RUN_SETUP = NOT_DONE\n')
-        if self.run_done:
+        if self.param.run_done:
             checklist_file.write('RUN = DONE\n')
         else:
             checklist_file.write('RUN = NOT_DONE\n')
         checklist_file.flush()
         checklist_file.close()
-        #absCfgFile_list = [os.path.abspath(cfgFile) for cfgFile in cfgFile_list]
 
+        call_args = []
+        call_args.append(sim_conf['IPS_ROOT'] + '/framework/src/ips.py')
+        if self.param.do_create_runspace:
+            call_args.append('--create-runspace')
+        if self.param.do_run_setup:
+            call_args.append('--run-setup')
+        if self.param.do_run:
+            call_args.append('--run')
+        
+        cfg_files_str = '--simulation=' + self.param.cfgFile_list[0]
+        for file in self.param.cfgFile_list[1:]:
+            cfg_files_str += ',' + file
+        call_args.append(cfg_files_str)
+
+        call_args.append('--platform=' + self.param.platform_filename)
+        call_args.append('--log=' + self.param.log_file)
+
+        print string.join(call_args, ' ')
         #test must return true if nothing bad happened, false otherwise
-        self.assertTrue(fwk.run(), 'error in running fwk')
+#       self.assertTrue(self.fwk.run(), 'error in running fwk')
+        self.assertEquals(subprocess.call(call_args), 0, 'error in running IPS')
 
         # set correct result of CREATE_RUNSPACE parameterization
-        if self.do_create_runspace or self.create_runspace_done:
+        if self.param.do_create_runspace or self.param.create_runspace_done:
             self.create_runspace_result = 'DONE'
         else:
             self.create_runspace_result = 'NOT_DONE'
 
         # set correct result of RUN_SETUP parameterization
-        if self.do_run_setup and self.create_runspace_result == 'DONE':
+        if self.param.do_run_setup and self.create_runspace_result == 'DONE':
             self.run_setup_result = 'DONE'
         else:
             self.run_setup_result = 'NOT_DONE'
             
         # set correct result of RUN parameterization
-        if self.do_run and self.run_setup_result == 'DONE':
+        if self.param.do_run and self.run_setup_result == 'DONE':
             self.run_result = 'DONE'
         else:
             self.run_result = 'NOT_DONE'
 
         # read in the values the Framework wrote out
-        conf = ConfigObj(checklist_file_name, 
+        self.conf = ConfigObj(checklist_file_name, 
                          interpolation = 'template',
                          file_error = True)
-        self.assertEquals(self.create_runspace_result, conf['CREATE_RUNSPACE'])
-        self.assertEquals(self.run_setup_result, conf['RUN_SETUP'])
-        self.assertEquals(self.run_result, conf['RUN'])
-        return 0
+
+        self.assertEquals(self.create_runspace_result, self.conf['CREATE_RUNSPACE'])
+        self.assertEquals(self.run_setup_result, self.conf['RUN_SETUP'])
+        self.assertEquals(self.run_result, self.conf['RUN'])
 
