fcec72fe6175b5909a6fe092b470810ab5fd9d17
kruger <kruger@ed2ff44e-3bad-47df-acac-40d95bfd9e26>
2011-11-02 04:55:31 +0000

    Forgot to add this

---
+++ trunk/tests/CmakeTests.cmake
@@ -0,0 +1,35 @@
+######################################################################
+#
+# This is a little snippet for all of the tests.
+#
+# $Id:$
+#
+######################################################################
+
+foreach (targ ${PYSCRIPTS})
+      add_test(${targ} python ${targ})
+endforeach ()
+foreach (targ ${SHSCRIPTS})
+      add_test(${targ} ${targ})
+endforeach ()
+
+install(PROGRAMS 
+    ${PYSCRIPTS} ${SHSCRIPTS}
+    DESTINATION bin/tests/bin
+    PERMISSIONS OWNER_READ OWNER_EXECUTE OWNER_WRITE
+                GROUP_READ GROUP_EXECUTE ${TX_GROUP_WRITE} ${TX_WORLD_PERMS}
+)
+
+if (NOT WIN32)
+get_filename_component(dirname ${CMAKE_CURRENT_BINARY_DIR} NAME_WE)
+add_custom_target(tests-${dirname}-scripts-stamp ALL
+    COMMAND ${CMAKE_SOURCE_DIR}/CMake/mklinks.sh txutils-scripts-stamp
+      ${CMAKE_CURRENT_SOURCE_DIR} ${DATA} ${PYSCRIPTS}
+    COMMAND ${CMAKE_SOURCE_DIR}/CMake/mklinks.sh txutils-scripts-stamp
+      ${CMAKE_CURRENT_SOURCE_DIR} ${DATA} ${SHSCRIPTS}
+    COMMAND chmod a+x *.py *.sh
+)
+else ()
+file(COPY ${PYSCRIPTS} DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
+file(COPY ${SHSCRIPTS} DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
+endif ()
