eb979665becb58461612a4e809d88f6427d2d287
Scott Kruger <kruger@txcorp.com>
2011-11-08 17:25:52 +0000

    basic_serial1.ips has been reworked to conform more to how the expected Composer usage will look like.  Then there is a bunch of fixes to get the runspace creation to work.  Still needs work on all of the other steps.

--- tests/refactor/test_ips.sh
+++ tests/refactor/test_ips.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
-ips --create-runspace --simulation=basicsim01 
-ips --run-setup --simulation=basicsim01 
-ips --run --simulation=basicsim01 
+source ../frameworkpath.py
+if test -d test_basic_serial1_0; then
+  /bin/rm -rf test_basic_serial1_0
+fi
+#-------------------------------------------------------
+echo "Testing runspace creation"
+#-------------------------------------------------------
+${fsrc}/ips.py --create-runspace --simulation=basic_serial1.ips
+exit
+#-------------------------------------------------------
+echo "Testing run setup"
+#-------------------------------------------------------
+${fsrc}/ips.py --run-setup --simulation=basicsim01 
+#-------------------------------------------------------
+echo "Testing running under ips"
+#-------------------------------------------------------
+${fsrc}/ips.py --run --simulation=basicsim01 
 #
-ips --clone=basicsim01 --simulation=basicsim02
-ips --run-setup --simulation=basicsim02
-ips --run --simulation=basicsim02
+#-------------------------------------------------------
+echo "Testing runspace creation"
+#-------------------------------------------------------
+${fsrc}/ips.py --clone=basicsim01 --simulation=basicsim02
+#-------------------------------------------------------
+echo "Testing runspace creation"
+#-------------------------------------------------------
+${fsrc}/ips.py --run-setup --simulation=basicsim02
+#-------------------------------------------------------
+echo "Testing runspace creation"
+#-------------------------------------------------------
+${fsrc}/ips.py --run --simulation=basicsim02
