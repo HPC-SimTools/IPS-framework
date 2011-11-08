3a8c312eb7e446cab6cee10f7877118c30fa4ecf
kruger <kruger@ed2ff44e-3bad-47df-acac-40d95bfd9e26>
2011-11-08 21:21:45 +0000

    Fixing some stupid mistakes.

--- trunk/tests/refactor/test_ips.sh
+++ trunk/tests/refactor/test_ips.sh
@@ -4,28 +4,34 @@ if test -d test_basic_serial1_0; then
   /bin/rm -rf test_basic_serial1_0
 fi
 #-------------------------------------------------------
+echo; echo; echo
 echo "Testing runspace creation"
 #-------------------------------------------------------
 ${fsrc}/ips.py --create-runspace --simulation=basic_serial1.ips
-exit
 #-------------------------------------------------------
+echo; echo; echo
 echo "Testing run setup"
 #-------------------------------------------------------
-${fsrc}/ips.py --run-setup --simulation=basicsim01 
+${fsrc}/ips.py --run-setup  --simulation=basic_serial1.ips
+exit
 #-------------------------------------------------------
+echo; echo; echo
 echo "Testing running under ips"
 #-------------------------------------------------------
-${fsrc}/ips.py --run --simulation=basicsim01 
+${fsrc}/ips.py --run --simulation=basic_serial1.ips
 #
 #-------------------------------------------------------
+echo; echo; echo
 echo "Testing runspace creation"
 #-------------------------------------------------------
-${fsrc}/ips.py --clone=basicsim01 --simulation=basicsim02
+${fsrc}/ips.py --clone=basicsim01  --simulation=basic_serial02
 #-------------------------------------------------------
+echo; echo; echo
 echo "Testing runspace creation"
 #-------------------------------------------------------
 ${fsrc}/ips.py --run-setup --simulation=basicsim02
 #-------------------------------------------------------
+echo; echo; echo
 echo "Testing runspace creation"
 #-------------------------------------------------------
 ${fsrc}/ips.py --run --simulation=basicsim02
