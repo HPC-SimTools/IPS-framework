51605645a4a5023321cbec47158da8c027567a9d
Ross Whitfield <whitfieldre@ornl.gov>
2020-08-07 13:28:49 -0400

    Remove sf-hosting and other random files

--- sf-hosting/htdocs/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-/* Redirect browser */
-header("Location: https://sourceforge.net/p/ipsframework/wiki/Home/");
-/* Make sure that code below does not get executed when we redirect. */
-exit;
-?>
