b85a11f4b176cdbbf2539355a5c518833a488423
Ross Whitfield <whitfieldre@ornl.gov>
2020-10-23 16:28:40 -0400

    Update example python scripts

--- doc/examples/skeleton_comp.py
+++ doc/examples/skeleton_comp.py
@@ -1,53 +1,53 @@
-#-------------------------------------------------------------------------------
-# Copyright 2006-2012 UT-Battelle, LLC. See LICENSE for more information.
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
+#  Copyright 2006-2012 UT-Battelle, LLC. See LICENSE for more information.
+# -------------------------------------------------------------------------------
 
 """
 This is a stripped down component that contains the boilerplate for creating a component and an outline of where other things go in comments.
 
 """
 
-#----------------------------------------------------------------------------
-# import modules
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+#  import modules
+# ----------------------------------------------------------------------------
 
-import os, sys         # the usuals
-import subprocess  # not used in skeleton, but useful for launching helper scripts
-from component import Component  # REQUIRED - all components inherit the IPS Component
+import os
+from ipsframework.component import Component  # REQUIRED - all components inherit the IPS Component
 
-class example (Component): # CHANGE CLASS NAME
+
+class example(Component):  # CHANGE CLASS NAME
 
     def __init__(self, services, config):
         Component.__init__(self, services, config)
-        print 'Created %s' % (self.__class__)
+        print('Created %s' % (self.__class__))
 
-#-----------------------------------------------------------------------------
-# init function
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+#  init function
+# -----------------------------------------------------------------------------
 
     def init(self, timeStamp=0):
         """
         Set up component initial state in order to do first step by copying and modifying input files in work directory.
         """
-        print 'example.init() called'  #CHANGE TO USE NEW CLASS NAME
-      
+        print('example.init() called')  # CHANGE TO USE NEW CLASS NAME
+
         services = self.services       # it is just more convenient to not type "self." all the time
-        
+
         # change to work directory for this component and return that location
-        workdir = services.get_working_dir()  
-      
+        workdir = services.get_working_dir()
+
         # Get global configuration parameters and set internal data needed for initialization work
 
         # Optional - setup component's log file
         self.example_log = os.path.join(workdir, 'log.example')        # CHANGE EXAMPLE TO COMPONENT NAME
 
         # Copy plasma state files over to working directory
-        # Get input files  
+        # Get input files
         # Run any helper scripts
         # Archive output files to output tree to save your work
 
         return 0
-        
+
 # ------------------------------------------------------------------------------
 #
 # RESTART function
@@ -55,47 +55,47 @@ class example (Component): # CHANGE CLASS NAME
 # Loads the global configuration parameters from the config file
 #
 # ------------------------------------------------------------------------------
-        
+
     def restart(self, timeStamp):
         """
         actions to restart from an IPS checkpoint
         """
-        print 'example.restart() called'
+        print('example.restart() called')
 
-        services = self.services
-        workdir = services.get_working_dir()
+        # services = self.services
+        # workdir = services.get_working_dir()
 
-        # Get restart files listed in config file.        
+        # Get restart files listed in config file.
         # Get global configuration parameters and set internal data
 
         return 0
-        
+
 # ------------------------------------------------------------------------------
 #
 # STEP function
 #
 # ------------------------------------------------------------------------------
-      
+
     def step(self, timeStamp):
         """
         Perform that action that this component is designed for.
 
-        In most cases that means running a computational model of some physical 
-        phenomena, along with all necessary data movement and helper programs to 
+        In most cases that means running a computational model of some physical
+        phenomena, along with all necessary data movement and helper programs to
         couple the data to the other components of the simulation.
         """
-        print 'example.step() called'              # CHANGE EXAMPLE TO COMPONENT NAME
+        print('example.step() called')              # CHANGE EXAMPLE TO COMPONENT NAME
 
-        services = self.services
-        workdir = services.get_working_dir()  
-      
+        # services = self.services
+        # workdir = services.get_working_dir()
+
         # Copy plasma state files over to working directory
-        # Get input files  
+        # Get input files
         # get the executables that will be run during step()
         # Call EXAMPLE prepare_input to generate the input files the executable is expecting
         # Launch EXAMPLE executable
         # check the return code or other mechanism to determine if the execution was successful
-        # Call process_output 
+        # Call process_output
         # Merge partial plasma state containing updated IC data
         # Archive output files
 
@@ -109,10 +109,9 @@ class example (Component): # CHANGE CLASS NAME
 # ------------------------------------------------------------------------------
 
     def checkpoint(self, timestamp=0.0):
-        print 'example.checkpoint() called'        # CHANGE EXAMPLE TO COMPONENT NAME
+        print('example.checkpoint() called')        # CHANGE EXAMPLE TO COMPONENT NAME
         services = self.services
         services.save_restart_files(timestamp, self.RESTART_FILES)
-        
 
 # ------------------------------------------------------------------------------
 #
@@ -122,5 +121,4 @@ class example (Component): # CHANGE CLASS NAME
 # ------------------------------------------------------------------------------
 
     def finalize(self, timestamp=0.0):
-        print 'example.finalize() called'        # CHANGE EXAMPLE TO COMPONENT NAME
-
+        print('example.finalize() called')        # CHANGE EXAMPLE TO COMPONENT NAME
