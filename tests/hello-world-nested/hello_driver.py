2f66f29b1efdb7ec564a805fa82f5acdea3af58b
Ross Whitfield <whitfieldre@ornl.gov>
2020-12-17 15:01:15 -0500

    Copy files from ips-examples

---
+++ tests/hello-world-nested/hello_driver.py
@@ -0,0 +1,28 @@
+#-------------------------------------------------------------------------------
+# Copyright 2006-2012 UT-Battelle, LLC. See LICENSE for more information.
+#-------------------------------------------------------------------------------
+
+from ipsframework import Component
+
+class HelloDriver(Component):
+    def __init__(self, services, config):
+        Component.__init__(self, services, config)
+        print('Created %s' % (self.__class__))
+
+    def init(self, timeStamp=0.0):
+        return
+
+    def validate(self, timeStamp=0.0):
+        return
+
+    def step(self, timeStamp=0.0):
+        try:
+            worker_comp = self.services.get_port('WORKER')
+        except Exception:
+            self.services.exception('Error accessing worker component')
+            raise
+        self.services.call(worker_comp, 'step', 0.0)
+        return
+        print('made it out of the worker call')
+    def finalize(self, timeStamp=0.0):
+        return
