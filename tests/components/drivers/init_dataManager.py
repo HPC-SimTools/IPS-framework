6a0a897ad66daa912c58208fefeb6bd2c608c2f6
Ross Whitfield <whitfieldre@ornl.gov>
2020-12-18 16:43:50 -0500

    Add test for DataManager stage_state and update_state

---
+++ tests/components/drivers/init_dataManager.py
@@ -0,0 +1,15 @@
+from ipsframework import Component
+
+
+class init_dataManager(Component):
+    def step(self, timestamp=0.0):
+        state_file_list = self.services.get_config_param('STATE_FILES').split(' ')
+
+        for state_file in state_file_list:
+            with open(state_file, 'w') as f:
+                if "100" in state_file:
+                    f.write("100\n")
+                else:
+                    f.write("1\n")
+
+        self.services.update_state()
