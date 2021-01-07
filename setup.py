dd5d9a5154482c348312b4eb40e8e2b6b88f1a6d
Ross Whitfield <whitfieldre@ornl.gov>
2021-01-07 10:22:15 -0500

    Update package info

--- setup.py
+++ setup.py
@@ -2,11 +2,39 @@
 from setuptools import setup, find_packages
 import versioneer
 
+long_description = """# Integrated Plasma Simulator (IPS) Framework
+
+The IPS was originally developed for the SWIM project and is designed
+for coupling plasma physics codes to simulate the interactions of
+various heating methods on plasmas in a tokamak. The physics goal of
+the project is to better understand how the heating changes the
+properties of the plasma and how these heating methods can be used to
+improve the stability of plasmas for fusion energy production.
+
+The IPS framework is thus designed to couple standalone codes flexibly
+and easily using python wrappers and file-based data coupling. These
+activities are not inherently plasma physics related and the IPS
+framework can be considered a general code coupling framework. The
+framework provides services to manage:
+
+ - the orchestration of the simulation through component invocation,
+    task launch and asynchronous event notification mechanisms
+ - configuration of complex simulations using familiar syntax
+ - file communication mechanisms for shared and internal (to a
+    component) data, as well as checkpoint and restart capabilities
+
+The framework performs the task, configuration, file and resource
+management, along with the event service, to provide these features.
+"""
+
 setup(
     name="ipsframework",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    url="https://github.com/HPC-SimTools/IPS-framework",
+    url="https://ips-framework.readthedocs.io",
+    description="Integrated Plasma Simulator (IPS) Framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     entry_points={
         'console_scripts': [
@@ -18,4 +46,5 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
+    python_requires='>=3.6'
 )
