da7d38cdd6b1adbed1407cbdd3f46809dce687f1
Lance-Drane <ldraneutk@gmail.com>
2024-05-30 09:32:24 -0400

    ignore simulation_setup directory
    Signed-off-by: Lance-Drane <ldraneutk@gmail.com>

--- examples-proposed/005-task-pool-sync/simulation_setup/driver.py
+++
@@ -1,8 +0,0 @@
-from ipsframework import Component
-
-
-class Driver(Component):
-    """In this instance, the driver allows for the worker component to handle all parallel orchestration."""
-    def step(self, timestamp=0.0):
-        worker_comp = self.services.get_port('WORKER')
-        self.services.call(worker_comp, 'step', 0.0)
