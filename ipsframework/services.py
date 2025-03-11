714d1023ddda880a02993e238b1ffdb714c88ad1
Mark Coletti <colettima@ornl.gov>
2025-03-11 18:52:58 -0400

    Turning off blocking when submitting tasks

--- ipsframework/services.py
+++ ipsframework/services.py
@@ -2337,7 +2337,7 @@ class ServicesProxy:
                           working_dir, 'ips.py', args)
 
         try:
-            num_submitted = self.submit_tasks(task_pool_name, block=True,
+            num_submitted = self.submit_tasks(task_pool_name, block=False,
                                               use_dask=False, dask_nodes=1,
                                               dask_ppw=None,
                                               launch_interval=0.0,
