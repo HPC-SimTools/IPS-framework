b3d9c4832ba645c545b1391cae63c0a4e1423ba3
Mark Coletti <colettima@ornl.gov>
2025-06-19 23:19:02 -0400

    Restored driver file

---
+++ doc/examples/ensembles/instance_driver.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+    Example driver for the ensembles example.
+
+    Please note that run_ensemble can be run from any IPS component, not
+    just a driver. The driver is used here for simplicity.
+"""
+from ipsframework import Component
+
+
+class instance_driver(Component):
+
+    def __init__(self, services, config):
+        super().__init__(services, config)
+        print('Creating instance driver')
+
+    def init(self, timeStamp=0.0):
+        return
+
+    def step(self, timestamp=0.0, **keywords):
+        """ Run the ensemble instance workers
+
+            In this example we have two components for an
+            example coupled simulation.  The components are
+            'a_sim_comp' and 'another_sim_comp'.  Here, we step
+            each of those components where they echo their
+            unique parameters.
+        """
+        self.services.info('Getting component ports')
+        a_comp = self.services.get_port('A_COMP')
+        another_comp = self.services.get_port('ANOTHER_COMP')
+
+        self.services.info('Stepping components')
+        self.services.call(a_comp, 'step', 0.0)
+        self.services.call(another_comp, 'step', 0.0)
+
+        self.services.info('Finished stepping components')
+
