d40a38a49c1fe075e41b0c4beb50e694f590f5ac
Lance-Drane <Lance-Drane@users.noreply.github.com>
2025-09-26 11:26:23 -0400

    rename ensemble header to portal
    Signed-off-by: Lance-Drane <Lance-Drane@users.noreply.github.com>

--- ipsframework/portalBridge.py
+++ ipsframework/portalBridge.py
@@ -161,7 +161,7 @@ def send_ensemble_variables(conn: Connection, stop: EventType, url: str, api_key
                     'Content-Type': 'text/csv',
                     'X-Ips-Username': username,
                     'X-Ips-Portal-Runid': next_val['portal_runid'],
-                    'X-Ips-Ensemble-Name': next_val['sim_name'],
+                    'X-Ips-Sim-Name': next_val['sim_name'],
                     'X-Ips-Ensemble-Id': next_val['ensemble_id'],
                 }
                 # TODO check to see that file size is < 1MB
