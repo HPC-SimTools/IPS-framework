4077fdaf39deb03d1b5fa4a3ad180730dc28dd1f
Mark Coletti <colettima@ornl.gov>
2025-10-20 22:51:03 -0400

    Added ensemble example for submitting a task from an instance component.

---
+++ examples-proposed/022-tasks-and-ensembles/README.md
@@ -0,0 +1,51 @@
+# Ensemble instances that launch tasks
+
+This shows an example of launching tasks from within ensemble instances. In 
+particular, each ensemble instance launches a task that runs a simple Python script
+that prints out the parameters passed to it and shows the run-time information
+such as the MPI rank, hostname, process ID, core affinities, start and stop 
+times.
+
+One difference from the 020 and 021 examples is that one of the instance 
+parameters, B, will be used control how long the task runs via a call to
+time.sleep().
+
+
+## Contents
+
+* `__init__.py` -- empty python init file
+* `driver.py` -- top-level driver
+* `instance_component.py` -- component worker code
+* `instance_driver.py` -- component driver code
+
+* `ensemble.conf` -- top-level configuration file
+* `platform.conf` -- platform configuration file
+* `perlmutter.conf` -- Perlmutter platform configuration file
+* `template.conf` -- ensemble instance configuration file
+
+* `perlmutter.slurm` -- SLURM submission script for Perlmutter
+
+
+## Instructions
+
+### Bash 
+To run the code, run:
+
+```bash
+ips.py --platform platform.conf --simulation ensemble.conf
+```
+__NOTE__: disable Global Protect VPN if you are using it, as it can interfere 
+with Dask.
+
+### Perlmutter
+
+To run on Perlmutter, follow these steps
+
+1. Create a conda environment 
+2. Install IPS into the conda environment
+3. Modify `perlmutter.slurm` to point to your conda environment and to use 
+   your project ID
+4. Run the example using the following command from within the directory:
+    ```bash
+     sbatch perlmutter.slurm
+    ```
---
+++ examples-proposed/022-tasks-and-ensembles/instance_component.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+    Component to be stepped in instance
+"""
+from time import time
+
+from ipsframework import Component
+
+
+class InstanceComponent(Component):
+
+    def step(self, timestamp: float = 0.0, **keywords):
+        start = time()
+
+        # ENSEMBLE_INSTANCE is a special IPS variable that contains the
+        # string uniquely identifying this instance.  Each instance will have
+        # the `run_ensemble()` `name` argument prepended to a unique number
+        # for each instance.  E.g., ENSEMBLE_INSTANCE might be "MY_INSTANCE_23".
+        instance_id = self.services.get_config_param('ENSEMBLE_INSTANCE')
+        self.services.info(f'{instance_id}: Start of step of instance '
+                           f'component.')
+
+        # Echo the parameters we're expecting, A, B, and C
+        self.services.info(f'{instance_id}: instance component parameters: '
+                           f'A={self.A}, B={self.B}, C={self.C}')
+
+        # We have to go three directory levels up because we're working off
+        # SIM_ROOT and the working directories that IPS constructed from there.
+        # I would recommend to reduce confusion to use absolute paths to
+        # any task executables.
+        mpi_executable = '../../../mpi_stats.py'
+        self.services.info(f'{instance_id}: Launching MPI executable: '
+                           f'{mpi_executable}')
+        args = ['-i', instance_id,
+                '-s', str(self.B), # arbitrarily using B to specify sleep time
+                '-o', 'stats.csv']
+        cmd = str(mpi_executable) + ' ' + ' '.join(args)
+        run_id = self.services.launch_task(nproc=1,
+                                           working_dir=str(Path('.').absolute()),
+                                           binary=cmd)
+
+        self.services.wait_task(run_id)  # block until done
+
+        self.services.info(f'{instance_id}: Completed MPI executable.')
+
+        self.services.info(f'{instance_id}: End of step of instance '
+                           f'component.')
