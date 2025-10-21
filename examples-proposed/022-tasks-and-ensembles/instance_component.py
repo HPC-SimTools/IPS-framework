4bf86370d6ed0f422085d9da303288be052b6c6f
Mark Coletti <colettima@ornl.gov>
2025-10-20 23:39:34 -0400

    Enhance MPI executable launch logging and add error handling

--- examples-proposed/022-tasks-and-ensembles/instance_component.py
+++ examples-proposed/022-tasks-and-ensembles/instance_component.py
@@ -29,15 +29,20 @@ class InstanceComponent(Component):
         # how many directory levels up to go to find the original python
         # script.
         mpi_executable = os.environ['MPI_STATS_EXEC']
-        self.services.info(f'{instance_id}: Launching MPI executable: '
-                           f'{mpi_executable}')
+        working_dir = str(Path('.').absolute())
+        self.services.info(f'{instance_id}: Launching MPI executable '
+                           f'{mpi_executable} in {working_dir}')
         args = ['-i', instance_id,
                 '-s', str(self.B), # arbitrarily using B to specify sleep time
                 '-o', 'stats.csv']
         cmd = str(mpi_executable) + ' ' + ' '.join(args)
-        run_id = self.services.launch_task(nproc=1,
-                                           working_dir=str(Path('.').absolute()),
-                                           binary=cmd)
+        try:
+            run_id = self.services.launch_task(nproc=1,
+                                               working_dir=working_dir,
+                                               binary=cmd)
+        except Exception as e:
+            self.services.critical(f'{instance_id}: Unable to launch '
+                                   f'{mpi_executable}')
 
         self.services.wait_task(run_id)  # block until done
 
