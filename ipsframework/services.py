03f4e62448d9959e114b8ef47ff7a0e5593f22cb
Mark Coletti <colettima@ornl.gov>
2025-11-11 16:17:53 -0500

    reverting to original prte invocation command line

--- ipsframework/services.py
+++ ipsframework/services.py
@@ -2659,8 +2659,8 @@ class DVMPlugin(WorkerPlugin):
 
         self.logger.info(f'Launching DVM')
         self.worker.dvm_uri_file = f'/tmp/dvm.uri.{os.getpid()}'
-        command = ['srun', '--mpi=pmix_v4', '-N', os.environ['SLURM_NNODES'],
-                   '--ntasks-per-node=1', 'prte', #'--no-daemonize',
+        command = [#'srun', '--mpi=pmix_v4', '-N', os.environ['SLURM_NNODES'], '--ntasks-per-node=1',
+                   'prte', #'--no-daemonize',
                    '--report-uri', self.worker.dvm_uri_file]
 
         mapping_policy = 'core'  # by default bind to cores
