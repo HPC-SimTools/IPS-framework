8532988695bdcf8b64925a12dffe64fb5aa720ea
Mark Coletti <colettima@ornl.gov>
2025-12-08 14:33:18 -0500

    Reducing resource allocation

--- examples-proposed/024-aggregated-compute-ensemble/instance_component.py
+++ examples-proposed/024-aggregated-compute-ensemble/instance_component.py
@@ -53,7 +53,7 @@ class InstanceComponent(Component):
         run_id = None
         try:
             cmd = ' '.join(cmd) # need one big ole string for executing tasks
-            run_id = self.services.launch_task(nproc=2,
+            run_id = self.services.launch_task(nproc=1,
                                                working_dir=working_dir,
                                                binary=cmd)
         except Exception as e:
