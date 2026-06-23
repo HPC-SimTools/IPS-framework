78d2f242492bcae92c018595a4d371c168e82f85
Mark Coletti <colettima@ornl.gov>
2026-06-23 11:55:34 -0400

    Tweaks to examples 020 and 021 to better show logfile and errfile behavior

--- examples-proposed/021-ensembles-from-CSV/driver.py
+++ examples-proposed/021-ensembles-from-CSV/driver.py
@@ -41,7 +41,9 @@ class EnsembleDriver(Component):
         # given set of variables.  E.g., the instance corresponding to
         # {'A' : 2, 'B' : 5.82, 'C' : 'baz'} is probably found in the
         # `INSTANCE_1` subdirectory.  We also direct the stdout and stderr
-        # for each instance to `my_logfile.txt`.
+        # for each instance to `my_logfile.txt`. I.e., by *not* specifying
+        # a filename for `errfile`, the stdout and stderr are combined into a
+        # single file for each instance.
         mapping = self.services.run_ensemble(template, variables,
                                              run_dir=Path('.').absolute(),
                                              name='INSTANCE_',
