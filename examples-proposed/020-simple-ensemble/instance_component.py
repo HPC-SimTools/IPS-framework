8283840cbe8731b6d2d0223431075a1fa480da7f
Mark Coletti <colettima@ornl.gov>
2026-06-23 12:02:51 -0400

    feat: add print statement for instance component parameters

--- examples-proposed/020-simple-ensemble/instance_component.py
+++ examples-proposed/020-simple-ensemble/instance_component.py
@@ -32,6 +32,8 @@ class InstanceComponent(Component):
         self.services.info(f'{instance_id}: instance component parameters: '
                            f'A={self.A}, B={self.B}, C={self.C}')
 
+        print(f'Values for {instance_id}: A={self.A}, B={self.B}, C={self.C}')
+
         # Save some per-component stats
         run_env = get_platform_info()
 
