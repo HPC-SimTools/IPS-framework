451abd830a4d306352b90ba82fd3ceb290e1e184
Mark Coletti <colettima@ornl.gov>
2026-09-24 21:30:37 -0400

    feat: add mpi_stats.py script for MPI rank and size reporting.  Example jobs now use name of example.

---
+++ examples-proposed/023-simple-ensemble-with-portal/mpi_stats.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+"""Echoes the MPI rank and size of the communicator, the hostname, the PID,
+affinity, and the number of cores. Also sleeps for a specified amount of time.
+"""
+
+import argparse
+import csv
+import os
+import socket
+import sys
+from time import sleep, time
+
+from mpi4py import MPI
+
+try:
+    import psutil
+except ImportError:
+    psutil = None
+
+if __name__ == '__main__':
+    start = time()
+
+    parser = argparse.ArgumentParser(description='MPI stats')
+    parser.add_argument('-i', '--id', type=str, default=str(os.getpid()), help='Task ID')
+    parser.add_argument('-s', '--sleep', default=5.0, type=float, help='Sleep time in seconds')
+    parser.add_argument('-o', '--output', type=str, default=None, help='Output CSV file')
+
+    args = parser.parse_args()
+
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+
+    hostname = socket.gethostname()
+    pid = os.getpid()
+
+    if psutil:
+        p = psutil.Process(pid)
+        if hasattr(p, 'cpu_affinity'):
+            affinity = p.cpu_affinity()
+            n_cores = len(affinity)
+        else:
+            affinity = None
+            n_cores = psutil.cpu_count()
+    else:
+        affinity = None
+        n_cores = os.cpu_count()  # fallback
+
+    print(
+        f'Rank {rank} of {size} in task {args.id} on {hostname} (pid {pid}) affinity {affinity!s} n_cores {n_cores} '
+    )
+
+    if args.sleep > 0:
+        print(f'Task {args.id} sleeping {args.sleep} seconds...')
+        sleep(args.sleep)
+
+    if args.output is not None:
+        with open(f'rank_{rank}_{args.output}', 'w', newline='') as csvfile:
+            fieldnames = [
+                'id',
+                'hostname',
+                'rank',
+                'size',
+                'pid',
+                'n_cores',
+                'affinity',
+                's',
+                'start',
+                'end',
+            ]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerow(
+                {
+                    'id': args.id,
+                    'hostname': hostname,
+                    'rank': rank,
+                    'size': size,
+                    'pid': pid,
+                    'n_cores': n_cores,
+                    'affinity': str(affinity),
+                    's': args.sleep,
+                    'start': start,
+                    'end': time(),
+                }
+            )
+
+    sys.exit(0)
